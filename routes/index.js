b4bdde7eacb7609e417a0dcffd8a826a48c35e2e
Shi Liu <liushie@gmail.com>
2016-05-12 14:50:57 +0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var sleep = require('sleep');
+var multiparty = require('multiparty')
+
+/* GET home page. */
+router.get('/weather.json', (req, res, next) => {
+	sleep.sleep(5)
+	res.json({'temperature': '11'})
+})
+
+router.post('/users', (req, res, next) => {
+	var form = new multiparty.Form()
+	form.parse(req, (err, fields, files)=> {
+		console.log(fields)
+		res.sendStatus(200)
+	})
+})
+
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
